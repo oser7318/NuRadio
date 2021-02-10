ebbc0d79ead7e513828abf6ed7d2428e408f2751
oser7318 <oscar.ericsson@live.com>
2021-02-10 22:02:33 +0100

    Confusion v. 1.3

--- Confusion.py
+++ Confusion.py
@@ -15,8 +15,7 @@ from tensorflow import keras
 from generator import TrainDataset, ValDataset, list_of_file_ids_val, n_events_per_file, n_files_train, n_files_val, batch_size, load_file
 from sklearn.metrics import confusion_matrix
 
-#Load saved model
-model=keras.models.load_model('/mnt/md0/analysis/flavor/01/saved_models/T01/model_best.h5')
+
 
 #Load data and labels
 
@@ -36,16 +35,18 @@ dataset_val = tf.data.Dataset.range(n_files_val).prefetch(n_batches_per_file * 1
 
 #Print some data
 for j in range(5):
-    print(dataset_val[i:1])
-    
+    print(dataset_val[j:1])
+
+#Load saved model
+#model=keras.models.load_model('/mnt/md0/analysis/flavor/01/saved_models/T01/model_best.h5')
+
 #Let model make predictions on validation dataset
-category_predictions = model.predict(dataset_val, batch_size=batch_size)
+#category_predictions = model.predict(dataset_val, batch_size=batch_size)
 
-print('Raw prediction values:')
+#print('Raw prediction values:')
 
-for i in category_predictions:
-    print(i)
+#for i in category_predictions:
+#    print(i)
 
-category_predictions = np.argmax(category_predictions, axis=1)
-
-print('np.argmax on prediction values:')
+#print('np.argmax on prediction values:')
+#category_predictions = np.argmax(category_predictions, axis=1)
