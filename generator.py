ecea4adabc7eff4de2c67a4528fed626c248eda8
oser7318 <oscar.ericsson@live.com>
2021-03-22 23:17:30 +0100

    load_file changes for new data

--- generator.py
+++ generator.py
@@ -5,7 +5,15 @@ import time
 
 np.set_printoptions(precision=4)
 
-datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband"
+#Category separated Noiseless data
+#datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/noiseless"
+
+#Category separated noise data
+datapath = " /mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/em_had_separately"
+
+#Original "old" data
+#datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband" 
+
 n_files = 10
 # n_files = 10
 n_files_test = 1
@@ -17,7 +25,7 @@ list_of_file_ids_train = np.arange(n_files_train, dtype=np.int)
 list_of_file_ids_val = np.arange(n_files_train, n_files_train + n_files_val, dtype=np.int)
 list_of_file_ids_test = np.arange(n_files_train + n_files_val, n_files, dtype=np.int)
 n_events_per_file = 100000
-batch_size = 64
+batch_size = 64 #Original value was 64
 
 print(f"training on {n_files_train} files ({n_files_train/n_files*100:.1f}%), validating on {n_files_val} files ({n_files_val/n_files*100:.1f}%), testing on {n_files_test} files ({n_files_test/n_files*100:.1f}%)")
 
@@ -37,39 +45,50 @@ def spherical_to_cartesian(zenith, azimuth):
         return np.array([x, y, z])
 
 
-def load_file(i_file, type='default' norm=norm):
+def load_file(i_file, noise=True, norm=norm):
 
-    if type == 'default':
-    #     t0 = time.time()
-        print(f"loading file {i_file}", flush=True)
-        data = np.load(os.path.join(datapath, f"data_1-3_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
-        labels_tmp = np.load(os.path.join(datapath, f"labels_1-3_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
-    #     print(f"finished loading file {i_file} in {time.time() - t0}s")
-    #     print(labels_tmp.item().keys())
-        shower_energy_em = np.array(labels_tmp.item()["shower_energy_em"])
-        mask = shower_energy_em == 0
 
-        label_onehot = np.zeros((len(shower_energy_em), 2), dtype=np.int)
-        label_onehot[mask, 0] = 1
-        label_onehot[~mask, 1] = 1
+    if noise == True:
+       #     t0 = time.time()
+       # print(f"loading file {i_file}", flush=True)
 
-    # check for nans and remove them
-        idx = ~(np.isnan(data))
-        idx = np.all(idx, axis=1)
-        idx = np.all(idx, axis=1)
-        idx = np.all(idx, axis=1)
-        data = data[idx, :, :, :]
-        data /= norm
-    #     print(f"finished processing file {i_file} in {time.time() - t0}s")
+	#EM+hadronic event data
+        datapath = " /mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/em_had_separately"
 
-        return data, label_onehot[idx, :]
-                
-    elif type == 'nonoise':
+	    #Hadronic event data
+		#data = np.load(os.path.join(datapath, f"data_had_emhad_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
+        #labels_tmp = np.load(os.path.join(datapath, f"labels_had_emhad_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
+        
+		
+	#"Old" data set, use together with corresponding datapath
+    elif noise == False:
+        datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/noiseless"
 
-        print(f"loading file {i_file}", flush=True)
+    data = np.load(os.path.join(datapath, f"data_emhad_emhad_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
+    labels = np.load(os.path.join(datapath, f"labels_emhad_emhad_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
 
-        data = np.load(os.path.join(datapath, f"data_simple2_nonoise_1-3_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
-        return data
+    return data, labels
+
+#		data = np.load(os.path.join(datapath, f"data_1-3_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
+#        labels_tmp = np.load(os.path.join(datapath, f"labels_1-3_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
+		
+#        shower_energy_em = np.array(labels_tmp.item()["shower_energy_em"])
+#       mask = shower_energy_em == 0
+#
+#	    label_onehot = np.zeros((len(shower_energy_em), 2), dtype=np.int)
+#	    label_onehot[mask, 0] = 1
+#      	label_onehot[~mask, 1] = 1
+#
+#    	# check for nans and remove them
+#        idx = ~(np.isnan(data))
+#        idx = np.all(idx, axis=1)
+#        idx = np.all(idx, axis=1)
+#        idx = np.all(idx, axis=1)
+#	    data = data[idx, :, :, :]
+#        data /= norm
+   	 #     print(f"finished processing file {i_file} in {time.time() - t0}s")
+
+#       	return data, label_onehot[idx, :]
 
 class TrainDataset(tf.data.Dataset):
 
@@ -97,7 +116,7 @@ class TrainDataset(tf.data.Dataset):
         return tf.data.Dataset.from_generator(
             cls._generator,
             output_types=(tf.dtypes.float64, tf.dtypes.float64),
-            output_shapes=((64, 5, 512, 1), (64, 2)),
+            output_shapes=((64, 5, 512, 1), (64, 2)), #Original was 64 to match batch size of 64 
             args=(file_id,)
         )
 
@@ -128,7 +147,7 @@ class ValDataset(tf.data.Dataset):
         return tf.data.Dataset.from_generator(
             cls._generator,
             output_types=(tf.dtypes.float64, tf.dtypes.float64),
-            output_shapes=((64, 5, 512, 1), (64, 2)),
+            output_shapes=((64, 5, 512, 1), (64, 2)), #Original was 64, increased batch size to see effects on training.
             args=(file_id,)
         )
 
