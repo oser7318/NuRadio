512522b2e2847425f1d053173c7d7b648e3801ae
oser7318 <oscar.ericsson@live.com>
2021-03-26 17:45:53 +0100

    New even datasets

--- generator.py
+++ generator.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import tensorflow as tf
 import time
+from sklearn.utils import shuffle
 
 np.set_printoptions(precision=4)
 
@@ -9,7 +10,7 @@ np.set_printoptions(precision=4)
 #datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/noiseless"
 
 #Category separated noise data
-datapath = " /mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/em_had_separately"
+#datapath = " /mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/em_had_separately"
 
 #Original "old" data
 #datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband" 
@@ -45,7 +46,7 @@ def spherical_to_cartesian(zenith, azimuth):
         return np.array([x, y, z])
 
 
-def load_file(i_file, noise=True, norm=norm):
+def load_file(i_file, noise=True, em=True, norm=norm):
 
 
     if noise == True:
@@ -53,63 +54,129 @@ def load_file(i_file, noise=True, norm=norm):
        # print(f"loading file {i_file}", flush=True)
 
 	#EM+hadronic event data
-        datapath = " /mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/em_had_separately"
+        datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/em_had_separately"
 
 	    #Hadronic event data
 		#data = np.load(os.path.join(datapath, f"data_had_emhad_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
         #labels_tmp = np.load(os.path.join(datapath, f"labels_had_emhad_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
-        
-		
-	#"Old" data set, use together with corresponding datapath
+
+
     elif noise == False:
         datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/ARZ2020_emhad_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/noiseless"
 
-    data = np.load(os.path.join(datapath, f"data_emhad_emhad_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
-    labels = np.load(os.path.join(datapath, f"labels_emhad_emhad_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
+    if em == True:
 
-    return data, labels
+        data = np.load(os.path.join(datapath, f"data_emhad_emhad_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
+        labels_tmp = np.load(os.path.join(datapath, f"labels_emhad_emhad_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
 
-#
+    elif em != True:
+
+        data = np.load(os.path.join(datapath, f"data_emhad_had_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
+        labels_tmp = np.load(os.path.join(datapath, f"labels_had_emhad_1-3_had_1_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
+        
+	#"Old" data set, use together with corresponding datapath
 #		data = np.load(os.path.join(datapath, f"data_1-3_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
-#        labels_tmp = np.load(os.path.join(datapath, f"labels_1-3_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
-		
-#        shower_energy_em = np.array(labels_tmp.item()["shower_energy_em"])
-#       mask = shower_energy_em == 0
-#
-#	    label_onehot = np.zeros((len(shower_energy_em), 2), dtype=np.int)
-#	    label_onehot[mask, 0] = 1
-#      	label_onehot[~mask, 1] = 1
-#
-#    	# check for nans and remove them
-#        idx = ~(np.isnan(data))
-#        idx = np.all(idx, axis=1)
-#        idx = np.all(idx, axis=1)
-#        idx = np.all(idx, axis=1)
-#	    data = data[idx, :, :, :]
-#        data /= norm
-   	 #     print(f"finished processing file {i_file} in {time.time() - t0}s")
+#       labels_tmp = np.load(os.path.join(datapath, f"labels_1-3_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
+    
+    shower_energy_em = np.array(labels_tmp.item()["shower_energy_em"])
+    mask = shower_energy_em == 0
 
-#       	return data, label_onehot[idx, :]
+    label_onehot = np.zeros((len(shower_energy_em), 2), dtype=np.int)
+    label_onehot[mask, 0] = 1
+    label_onehot[~mask, 1] = 1
 
-class TrainDataset(tf.data.Dataset):
+    	# check for nans and remove them
+    idx = ~(np.isnan(data))
+    idx = np.all(idx, axis=1)
+    idx = np.all(idx, axis=1)
+    idx = np.all(idx, axis=1)
+    data = data[idx, :, :, :]
+    data /= norm
+    
+    return data, label_onehot[idx, :]
+
+#Shuffle function that alters states of a and b in the same way, no copies created.
+def shuffle_same(a,b):
+    
+    rng_state = numpy.random.get_state()
+    numpy.random.shuffle(a)
+    numpy.random.set_state(rng_state)
+    numpy.random.shuffle(b)      
+
+class TrainDatasetNoiseless(tf.data.Dataset):
 
     def _generator(file_id):
-#         print(f"\nTrain generator current id {file_id}, opening file {list_of_file_ids_train[file_id]}")
         if((file_id + 1) == n_files_train):
 #             print("reshuffling")
             np.random.shuffle(list_of_file_ids_train)
 
-        # Opening the file
         i_file = list_of_file_ids_train[file_id]
-        data, nu_direction = load_file(i_file, norm)
-        num_samples = data.shape[0]
+        data_had, labels_had = load_file(i_file, noise=False, em=False, norm=norm)
+        data_emhad, labels_emhad = load_file(i_file, noise=False, em=True, norm=norm) 
+
+        #Joint data array (not shuffeled and doubble size of constituent data)
+        data_combined = np.concatenate( (data_had, data_emhad), axis=0)
+        labels_combined = np.concatenate( (labels_had, labels_emhad), axis=0)
+              
+        #Shuffle using shuffle_same(a,b)
+        shuffle_same(data_combined,labels_combined)
+
+        #Shuffle using sklearn.utils.shuffle, which leaves the imput array intact (creates a copy but shuffeled)
+        #data_combined, labels_combined = shuffle(data_combined, labels_combined, random_state=0)
+
+        #Cut the combined data array in half? / Save only first 100 000 events in combined array? DO RUNS WITH CUT (100k) AND 200k EVENT ARRAYS AND SEE WHAT HAPPENS!
+        
+        num_samples = data_combined.shape[0]
         rand_ids = np.arange(num_samples, dtype=np.int)
         np.random.shuffle(rand_ids)
         n_batches = num_samples // batch_size
         for i_batch in range(n_batches):
             # Reading data (line, record) from the file
-            y = nu_direction[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size]]
-            x = data[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size], :, :, :]
+            y = labels_combined[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size]]
+            x = data_combined[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size], :, :, :]
+            yield x, y
+
+    def __new__(cls, file_id):
+#         print(f"input arg {tmp}, {batch_size}")
+        return tf.data.Dataset.from_generator(
+            cls._generator,
+            output_types=(tf.dtypes.float64, tf.dtypes.float64),
+            output_shapes=((64, 5, 512, 1), (64, 2)), #Original was 64 to match batch size of 64 
+            args=(file_id,)
+        )
+    
+
+class ValDatasetNoiseless(tf.data.Dataset):
+
+    def _generator(file_id):
+        if((file_id + 1) == n_files_val):
+#             print("reshuffling")
+            np.random.shuffle(list_of_file_ids_val)
+
+        i_file = list_of_file_ids_val[file_id]
+        data_had, labels_had = load_file(i_file, noise=False, em=False, norm=norm)
+        data_emhad, labels_emhad = load_file(i_file, noise=False, em=True, norm=norm) 
+
+        #Joint data array (not shuffeled and doubble size of constituent data)
+        data_combined = np.concatenate( (data_had, data_emhad), axis=0)
+        labels_combined = np.concatenate( (labels_had, labels_emhad), axis=0)
+              
+        #Shuffle using shuffle_same(a,b)
+        shuffle_same(data_combined,labels_combined)
+
+        #Shuffle using sklearn.utils.shuffle, which leaves the imput array intact (creates a copy but shuffeled)
+        #data_combined, labels_combined = shuffle(data_combined, labels_combined, random_state=0)
+
+        #Cut the combined data array in half? / Save only first 100 000 events in combined array? DO RUNS WITH CUT (100k) AND 200k EVENT ARRAYS AND SEE WHAT HAPPENS!
+        
+        num_samples = data_combined.shape[0]
+        rand_ids = np.arange(num_samples, dtype=np.int)
+        np.random.shuffle(rand_ids)
+        n_batches = num_samples // batch_size
+        for i_batch in range(n_batches):
+            # Reading data (line, record) from the file
+            y = labels_combined[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size]]
+            x = data_combined[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size], :, :, :]
             yield x, y
 
     def __new__(cls, file_id):
@@ -121,34 +188,66 @@ class TrainDataset(tf.data.Dataset):
             args=(file_id,)
         )
 
+#ctrl+k+u to uncomment
 
-class ValDataset(tf.data.Dataset):
+# class TrainDataset(tf.data.Dataset):
 
-    def _generator(file_id):
-#         print(f"\nVal generator current id {file_id}, opening file {list_of_file_ids_val[file_id]}")
-        if((file_id + 1) == n_files_val):
-            # print("reshuffling")
-            np.random.shuffle(list_of_file_ids_val)
+#     def _generator(file_id):
+# #         print(f"\nTrain generator current id {file_id}, opening file {list_of_file_ids_train[file_id]}")
+#         if((file_id + 1) == n_files_train):
+# #             print("reshuffling")
+#             np.random.shuffle(list_of_file_ids_train)
 
-        # Opening the file
-        i_file = list_of_file_ids_val[file_id]
-        data, nu_direction = load_file(i_file, norm)
-        num_samples = data.shape[0]
-        rand_ids = np.arange(num_samples, dtype=np.int)
-        np.random.shuffle(rand_ids)
-        n_batches = num_samples // batch_size
-        for i_batch in range(n_batches):
-            # Reading data (line, record) from the file
-            y = nu_direction[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size]]
-            x = data[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size], :, :, :]
-            yield x, y
+#         # Opening the file
+#         i_file = list_of_file_ids_train[file_id]
+#         data, nu_direction = load_file(i_file, norm)
+#         num_samples = data.shape[0]
+#         rand_ids = np.arange(num_samples, dtype=np.int)
+#         np.random.shuffle(rand_ids)
+#         n_batches = num_samples // batch_size
+#         for i_batch in range(n_batches):
+#             # Reading data (line, record) from the file
+#             y = nu_direction[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size]]
+#             x = data[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size], :, :, :]
+#             yield x, y
 
-    def __new__(cls, file_id):
-#         print(f"input arg {tmp}, {batch_size}")
-        return tf.data.Dataset.from_generator(
-            cls._generator,
-            output_types=(tf.dtypes.float64, tf.dtypes.float64),
-            output_shapes=((64, 5, 512, 1), (64, 2)), #Original was 64, increased batch size to see effects on training.
-            args=(file_id,)
-        )
+#     def __new__(cls, file_id):
+# #         print(f"input arg {tmp}, {batch_size}")
+#         return tf.data.Dataset.from_generator(
+#             cls._generator,
+#             output_types=(tf.dtypes.float64, tf.dtypes.float64),
+#             output_shapes=((64, 5, 512, 1), (64, 2)), #Original was 64 to match batch size of 64 
+#             args=(file_id,)
+#         )
+
+
+# class ValDataset(tf.data.Dataset):
+
+#     def _generator(file_id):
+# #         print(f"\nVal generator current id {file_id}, opening file {list_of_file_ids_val[file_id]}")
+#         if((file_id + 1) == n_files_val):
+#             # print("reshuffling")
+#             np.random.shuffle(list_of_file_ids_val)
+
+#         # Opening the file
+#         i_file = list_of_file_ids_val[file_id]
+#         data, nu_direction = load_file(i_file, norm)
+#         num_samples = data.shape[0]
+#         rand_ids = np.arange(num_samples, dtype=np.int)
+#         np.random.shuffle(rand_ids)
+#         n_batches = num_samples // batch_size
+#         for i_batch in range(n_batches):
+#             # Reading data (line, record) from the file
+#             y = nu_direction[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size]]
+#             x = data[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size], :, :, :]
+#             yield x, y
+
+#     def __new__(cls, file_id):
+# #         print(f"input arg {tmp}, {batch_size}")
+#         return tf.data.Dataset.from_generator(
+#             cls._generator,
+#             output_types=(tf.dtypes.float64, tf.dtypes.float64),
+#             output_shapes=((64, 5, 512, 1), (64, 2)), #Original was 64, increased batch size to see effects on training.
+#             args=(file_id,)
+#         )
 
